8fff986c9d0f98a3d0ce010a9683afab945efbcd
Rasmus Kibshede <2740ralle@gmail.com>
2021-04-14 11:40:44 +0200

    Added class Student

---
+++ 7PointGradingScale/src/Student.java
@@ -0,0 +1,14 @@
+public class Student {
+
+  String name;
+  int[] grades;
+
+  public Student(String name, int[] grades) {
+    this.name = name;
+    this.grades = grades;
+  }
+
+  public double averageGrade() {
+    return 0;
+  }
+}
