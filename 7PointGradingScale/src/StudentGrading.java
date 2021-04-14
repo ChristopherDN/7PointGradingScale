e33d48bae52c741cb15cccd26ddd108a06da3f27
christophernielsen <AcnCatering@gmail.com>
2021-04-14 11:35:27 +0200

    Scanner, array and for loop

--- 7PointGradingScale/src/StudentGrading.java
+++ 7PointGradingScale/src/StudentGrading.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class StudentGrading {
 
+  public static void main(String[] args) {
+    int[] grades = new int[6];
+    Scanner in = new Scanner(System.in);
+    for (int i = 0; i < 7; i++) {
+      System.out.println("Enter your grade");
+      grades[i] = in.nextInt();
+    }
+
+
+
+  }
 }
